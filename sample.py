a9a32450dc6561c219d1f4fbcbba9dc8c65fe04f
Hashmat02 <157471281+Hashmat02@users.noreply.github.com>
2024-11-14 21:51:34 +0500

    Sampling English-Urdu Q/A pairs
    Randomly sampling and merging Q/A pairs in both english and urdu based on question ID

---
+++ sample.py
@@ -0,0 +1,25 @@
+#randomly sampliing 20 rows from the merged data and saving it in a new excel file
+import pandas as pd
+
+english_df = pd.read_excel('/Users/hashmat/Desktop/LLM project/squad2_English.xlsx')
+urdu_df = pd.read_excel('/Users/hashmat/Desktop/LLM project/squad2_Urdu.xlsx')
+
+merged_df = pd.merge(english_df, urdu_df, on='id', suffixes=('_English', '_Urdu'))
+
+print("Columns after merging:", merged_df.columns)
+
+sampled_df = merged_df.sample(n=20, random_state=66)
+
+final_df = pd.DataFrame({
+    'ID': sampled_df['id'],
+    'English Question': sampled_df['question_English'], 
+    'English Answer': sampled_df['answers'], 
+    'English Title': sampled_df['title_English'], 
+    'Urdu Question': sampled_df['question_Urdu'], 
+    'Urdu Answer': sampled_df['answer'], 
+    'Urdu Title': sampled_df['title_Urdu']  
+})
+
+final_df.to_excel('/Users/hashmat/Desktop/LLM project/squad2_English_Urdu_Pairs.xlsx', index=False)
+
+print("New Excel file 'squad2_English_Urdu_Pairs.xlsx' created with 20 randomly selected question pairs.")
