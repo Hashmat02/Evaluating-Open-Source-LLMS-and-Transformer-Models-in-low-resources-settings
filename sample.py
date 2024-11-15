de3955ecfb50e13508b354e8b7c5185e611eb687
Hashmat02 <157471281+Hashmat02@users.noreply.github.com>
2024-11-15 16:56:43 +0500

    Sample Pseudo Attack dataset

--- sample.py
+++ sample.py
@@ -1,11 +1,9 @@
-#randomly sampliing 20 rows from the merged data and saving it in a new excel file
 import pandas as pd
 
 english_df = pd.read_excel('/Users/hashmat/Desktop/LLM project/squad2_English.xlsx')
 urdu_df = pd.read_excel('/Users/hashmat/Desktop/LLM project/squad2_Urdu.xlsx')
 
 merged_df = pd.merge(english_df, urdu_df, on='id', suffixes=('_English', '_Urdu'))
-
 print("Columns after merging:", merged_df.columns)
 
 sampled_df = merged_df.sample(n=20, random_state=66)
@@ -13,11 +11,13 @@ sampled_df = merged_df.sample(n=20, random_state=66)
 final_df = pd.DataFrame({
     'ID': sampled_df['id'],
     'English Question': sampled_df['question_English'], 
-    'English Answer': sampled_df['answers'], 
-    'English Title': sampled_df['title_English'], 
+    'English Answer': sampled_df['answers'],  
+    'English Title': sampled_df['title_English'],  
+    'English Context': sampled_df['context_English'], 
     'Urdu Question': sampled_df['question_Urdu'], 
-    'Urdu Answer': sampled_df['answer'], 
-    'Urdu Title': sampled_df['title_Urdu']  
+    'Urdu Answer': sampled_df['answer'],  
+    'Urdu Title': sampled_df['title_Urdu'],  
+    'Urdu Context': sampled_df['context_Urdu']  
 })
 
 final_df.to_excel('/Users/hashmat/Desktop/LLM project/squad2_English_Urdu_Pairs.xlsx', index=False)
